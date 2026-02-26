55556008f12c1332fc967c16b731cf27736a0d77
Nusulukbekov <nrsltnulukbekov@gmail.com>
2026-02-26 15:01:27 +0600

    updated css

--- hash.js
+++ hash.js
@@ -7,4 +7,4 @@ bcrypt.hash("123456", 10).then(hash => {
 // psql "postgresql://postgres:GmKPFqUxbPzYGnHOjRUtOCrHwxGrXKma@gondola.proxy.rlwy.net:52009/railway"
 // INSERT INTO users (username, password, role)
 // VALUES ('admin', '$2b$10$Z6Rw..6u/Ka7vR9FJ3o8M.qxQRQ5tBBX5fV.BAIP/COcYmMGKv1zu', 'admin');
-//hashh
+//hashh \d vehicles
